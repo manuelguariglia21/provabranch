8d9641bf0c39ea59881f96b86fbeb5e7d822ea52
andreafontaneto <andrea.fontaneto@gmail.com>
2022-02-22 11:34:30 +0100

    make premium plan table + migration

--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/matteodeias/Documents/Boolean/progetto finale/provabranch/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/matteodeias/Documents/Boolean/progetto finale/provabranch/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\andre\OneDrive\Documenti\Boolean\Progetto Finale\provabranch\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\andre\OneDrive\Documenti\Boolean\Progetto Finale\provabranch\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
