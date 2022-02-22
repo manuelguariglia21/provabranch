2c88d7888ce3cde83b17e1948e4d155a824973e8
GinoGreen <luigiverde97@gmail.com>
2022-02-22 12:37:40 +0100

    relationship many to many

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function job_roles() {
+        return $this->belongsToMany('App\Job_role');
+    }
 }
